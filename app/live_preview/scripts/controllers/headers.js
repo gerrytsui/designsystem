d74f89277ce9d1e1a4021a04b79721cb5d55369b
lombardi-gux <steve.lombardi@pb.com>
2014-05-07 10:37:13 -0400

    changing file structure drastically

--- app/live_preview/scripts/controllers/headers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('designSystemApp').controller('HeadersCtrl', ['$scope', 'FakeNavFactory',
-  function($scope, FakeNavFactory) {
-
-    $scope.fakeNavData = FakeNavFactory.getFakeNavData();
-
-  }
-]);
--- app/live_preview/scripts/filters/capitalize.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-angular.module('ngFilters', []).filter('capitalize', function() {
-  return function(input) {
-    return input.charAt(0).toUpperCase() + input.slice(1);
-  };
-});
